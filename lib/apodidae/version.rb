c929ae49d180e10672fd9eed592a3356c5d096c8
mathfuru <mathfuru@gmail.com>
2012-12-08 21:39:10 +0900

    Add --help, --watch and --version option to apodidae command. (Fix #9)

---
+++ lib/apodidae/version.rb
@@ -0,0 +1,3 @@
+module Apodidae
+  VERSION = '0.0.0'
+end
--- spec/integration/apodidae_command_spec.rb
+++ spec/integration/apodidae_command_spec.rb
@@ -5,4 +5,65 @@ describe 'apodidae command' do
   it "can be run" do
     IO.popen(["#{BASE_DIR}/bin/apodidae"],  'r+')
   end
+
+  describe 'with --help option' do
+    it "output help statement" do
+      execute_apodidae_command("--help").should == <<EOS
+Usage: apodidae [options]
+    -w, --watch     compile automatically when template or source is changed.
+EOS
+    end
+  end
+
+  describe 'with -h option' do
+    it "output help statement" do
+      execute_apodidae_command("-h").should == <<EOS
+Usage: apodidae [options]
+    -w, --watch     compile automatically when template or source is changed.
+EOS
+    end
+  end
+
+  describe "with --version option" do
+    it "output version number" do
+      execute_apodidae_command("--version").strip.should == '0.0.0'
+    end
+  end
+
+  describe "with -v option" do
+    it "output version number" do
+      execute_apodidae_command("-v").strip.should == '0.0.0'
+    end
+  end
+
+  describe "with --watch option" do
+    it "output 'before implementation'" do
+      execute_apodidae_command("--watch").should be_include('before implementation')
+    end
+  end
+
+  describe "with -w option" do
+    it "output 'before implementation'" do
+      execute_apodidae_command("-w").should be_include('before implementation')
+    end
+  end
+
+  describe "without options" do
+    pending
+#    it "output usage" do
+#      execute_apodidae_command.should == <<EOS
+#Usage: apodidae [options]
+#    -w, --watch     compile automatically when template or source is changed.
+#EOS
+#    end
+  end
+
+  def execute_apodidae_command(*args)
+    args = [args].flatten
+    stdout_string = nil
+    IO.popen(["#{BASE_DIR}/bin/apodidae"]+args, 'r+') do |io|
+      stdout_string = io.read if io
+    end
+    stdout_string
+  end
 end
