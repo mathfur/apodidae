d8125f3c9d96feb8c903a821792ebfb51e4399d7
mathfuru <mathfuru@gmail.com>
2012-12-16 07:53:01 +0900

    rafactoring combine

--- spec/units/combine_spec.rb
+++ spec/units/combine_spec.rb
@@ -3,46 +3,52 @@ require "spec_helper"
 
 describe Apodidae do
   describe '#initialize' do
-    before do
-      @barb = Apodidae::Barb.new(:table,<<EOS)
-substitute(
-  :users_table => :html_id
-)
-html do
-  tag :table, id: 'users_table'  do
-  end
-end
-EOS
+    describe 'with no loop' do
+      before do
+        @barb = Apodidae::Barb.new(:table,<<-EOS)
+          substitute(
+            :users_table => :html_id
+          )
+          html do
+            tag :table, id: 'users_table'  do
+            end
+          end
+        EOS
+      end
+
+      describe 'when rachis have right name' do
+        before { @rachis = Apodidae::Rachis.new(:table,html_id: 'foo_table') }
+        subject { @combine = Apodidae::Combine.new([@barb], @rachis) }
+
+        it 'keys in barb statement are replaced by substitute block' do
+          subject.instance_variable_get(:@result).gsub(/\s+/, ' ').should == <<-EOS.gsub(/\s+/, ' ')
+            html do
+              tag :table, id: 'foo_table'  do
+              end
+            end
+          EOS
+        end
+      end
+
+      describe 'when rachis have wrong barb name' do
+        before { @rachis = Apodidae::Rachis.new(:wrong_barb_name,html_id: 'foo_table') }
+        specify { Proc.new { Apodidae::Combine.new([@barb], @rachis) }.should raise_error(RuntimeError) }
+      end
     end
 
-    describe 'The first argument is :table and the second is {html_id: "foo_table"}' do
-      before do
-        @rachis = Apodidae::Rachis.new(:table,html_id: 'foo_table')
-      end
-
-      subject do
-        @combine = Apodidae::Combine.new([@barb], @rachis)
-      end
-
-      specify do
-        subject.instance_variable_get(:@result).should == <<EOS
-html do
-  tag :table, id: 'foo_table'  do
-  end
-end
-EOS
-      end
-    end
-
-    describe 'not existing barb name is specified' do
-      before do
-        @rachis = Apodidae::Rachis.new(:table_wrong,html_id: 'foo_table')
-      end
-
-      specify do
-        Proc.new do
-          @combine = Apodidae::Combine.new([@barb], @rachis)
-        end.should raise_error(RuntimeError)
+    describe 'with loop' do
+      describe 'loop block is expanded by row statement' do
+        before do
+          @barb = Apodidae::Barb.new(:table,<<-EOS)
+            substitute(
+              :users_table => :html_id
+            )
+            html do
+              tag :table, id: 'users_table'  do
+              end
+            end
+          EOS
+        end
       end
     end
   end
