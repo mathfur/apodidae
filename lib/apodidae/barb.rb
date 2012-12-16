37d0ea0b88f6b6f8d93a78cc56ebf6e4ed354082
mathfuru <mathfuru@gmail.com>
2012-12-16 10:18:47 +0900

    modify substitute hash to block in barb

--- lib/apodidae/barb.rb
+++ lib/apodidae/barb.rb
@@ -7,22 +7,53 @@ module Apodidae
     def initialize(name, contents)
       @name = name.to_s
       @contents = contents
+      @sandbox = BarbSandbox.new
     end
 
     def substitute_rules
-      sandbox = BarbSandbox.new
-      sandbox.instance_eval(@contents)
-      sandbox.src_and_dst
+      @sandbox.instance_eval(@contents)
+      @sandbox.src_and_dst
+    end
+
+    def add_rachis_attrs(names_and_values)
+      @sandbox.substitute_sandbox.add_rachis_attrs(names_and_values)
+      self
     end
   end
 
   class BarbSandbox
-    attr_reader :src_and_dst
-    def substitute(src_and_dst={})
-      @src_and_dst = src_and_dst
+    attr_reader :src_and_dst, :substitute_sandbox
+    def method_missing(*args); end
+
+    def initialize
+      @substitute_sandbox = SubstituteSandbox.new
     end
 
-    def method_missing(*args)
+    def substitute(&block)
+      @substitute_sandbox.instance_eval(&block)
+      @src_and_dst = @substitute_sandbox.hash
+    end
+  end
+
+  class SubstituteSandbox
+    attr_reader :hash
+
+    def initialize
+      @hash = {}
+    end
+
+    def method_missing(name, *args)
+      @hash[name] = args
+    end
+
+    def add_rachis_attrs(names_and_values)
+      self.class.class_eval do
+        names_and_values.each do |k, v|
+          define_method k do
+            return v
+          end
+        end
+      end
     end
   end
 end
--- spec/units/barb_spec.rb
+++ spec/units/barb_spec.rb
@@ -2,38 +2,88 @@
 require "spec_helper"
 
 describe Apodidae do
-  describe 'when normal argument is specified' do
-    subject { Apodidae::Barb.new('foo',<<EOS) }
-substitute(
-  :abc => 123
-)
+  describe '#initialize' do
+    describe 'when normal argument is specified' do
+      subject { Apodidae::Barb.new('foo',<<EOS) }
+substitute do
+  abc 123
+end
 EOS
-    specify { subject.name.should == "foo" }
-    specify { subject.contents.should == <<EOS }
-substitute(
-  :abc => 123
-)
+      specify { subject.name.should == "foo" }
+      specify { subject.contents.should == <<EOS }
+substitute do
+  abc 123
+end
 EOS
-    specify do
-      subject.substitute_rules.should == {:abc => 123}
+      specify do
+        subject.substitute_rules.should == {:abc => [123]}
+      end
     end
-  end
 
-  describe 'when the contents have general methods' do
-    subject { Apodidae::Barb.new('foo',<<EOS) }
-substitute(
-  :abc => 123
-)
+    describe 'when the contents have general methods' do
+      subject { Apodidae::Barb.new('foo',<<EOS) }
+substitute do
+  abc 123
+end
 html do
 end
 EOS
-    specify { subject.name.should == "foo" }
-    specify { subject.contents.should == <<EOS }
-substitute(
-  :abc => 123
-)
+      specify { subject.name.should == "foo" }
+      specify { subject.contents.should == <<EOS }
+substitute do
+  abc 123
+end
 html do
 end
 EOS
+    end
+
+    describe 'when the contents have general methods' do
+      subject { Apodidae::Barb.new('foo',<<EOS) }
+substitute do
+  abc 123
+end
+html do
+end
+EOS
+      specify { subject.name.should == "foo" }
+      specify { subject.contents.should == <<EOS }
+substitute do
+  abc 123
+end
+html do
+end
+EOS
+    end
+
+    describe do
+      subject do
+        barb = Apodidae::Barb.new('foo', <<EOS)
+substitute do
+  abc x
+end
+EOS
+        barb.add_rachis_attrs(:x => 15)
+      end
+
+      specify { subject.substitute_rules.should == {:abc => [15]} }
+    end
+
+    describe do
+      it "labelありの場合" do
+        pending
+        subject { Apodidae::Barb.new('foo', <<EOS) }
+substitute do
+  __label def
+  abc 123
+end
+html do
+end
+EOS
+        specify { subject.name.should == 'foo' }
+        specify { subject.substitute_rules.should == {:abc => {:def => 123}} }
+              # abc.defのようにアクセスできるようにするべき?
+      end
+    end
   end
 end
