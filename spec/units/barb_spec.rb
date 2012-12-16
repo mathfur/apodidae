b0b2d9ef963721537c5964c979b59bd2e34ef353
mathfuru <mathfuru@gmail.com>
2012-12-16 17:03:13 +0900

    Refactor barb

--- spec/units/barb_spec.rb
+++ spec/units/barb_spec.rb
@@ -4,65 +4,65 @@ require "spec_helper"
 describe Apodidae do
   describe '#initialize' do
     describe 'when normal argument is specified' do
-      subject { Apodidae::Barb.new('foo',<<EOS) }
-substitute do
-  abc 123
-end
-EOS
+      subject { Apodidae::Barb.new('foo',<<-EOS) }
+        substitute do
+          abc 123
+        end
+        EOS
       specify { subject.name.should == "foo" }
-      specify { subject.contents.should == <<EOS }
-substitute do
-  abc 123
-end
-EOS
+      specify { subject.contents.should == <<-EOS }
+        substitute do
+          abc 123
+        end
+        EOS
       specify do
         subject.substitute_rules.should == {:abc => [123]}
       end
     end
 
     describe 'when the contents have general methods' do
-      subject { Apodidae::Barb.new('foo',<<EOS) }
-substitute do
-  abc 123
-end
-html do
-end
-EOS
+      subject { Apodidae::Barb.new('foo',<<-EOS) }
+        substitute do
+          abc 123
+        end
+        html do
+        end
+        EOS
       specify { subject.name.should == "foo" }
-      specify { subject.contents.should == <<EOS }
-substitute do
-  abc 123
-end
-html do
-end
-EOS
+      specify { subject.contents.should == <<-EOS }
+        substitute do
+          abc 123
+        end
+        html do
+        end
+        EOS
     end
 
     describe 'when the contents have general methods' do
-      subject { Apodidae::Barb.new('foo',<<EOS) }
-substitute do
-  abc 123
-end
-html do
-end
-EOS
+      subject { Apodidae::Barb.new('foo',<<-EOS) }
+        substitute do
+          abc 123
+        end
+        html do
+        end
+        EOS
       specify { subject.name.should == "foo" }
-      specify { subject.contents.should == <<EOS }
-substitute do
-  abc 123
-end
-html do
-end
-EOS
+      specify { subject.contents.should == <<-EOS }
+        substitute do
+          abc 123
+        end
+        html do
+        end
+        EOS
     end
 
     describe do
       subject do
-        barb = Apodidae::Barb.new('foo', <<EOS)
-substitute do
-  abc x
-end
-EOS
+        barb = Apodidae::Barb.new('foo', <<-EOS)
+          substitute do
+            abc x
+          end
+          EOS
         barb.add_rachis_attrs(:x => 15)
       end
 
@@ -72,14 +72,14 @@ EOS
     describe do
       it "labelありの場合" do
         pending
-        subject { Apodidae::Barb.new('foo', <<EOS) }
-substitute do
-  __label def
-  abc 123
-end
-html do
-end
-EOS
+        subject { Apodidae::Barb.new('foo', <<-EOS) }
+          substitute do
+            __label def
+            abc 123
+          end
+          html do
+          end
+          EOS
         specify { subject.name.should == 'foo' }
         specify { subject.substitute_rules.should == {:abc => {:def => 123}} }
               # abc.defのようにアクセスできるようにするべき?
