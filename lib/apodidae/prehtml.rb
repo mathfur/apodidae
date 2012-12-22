9a0d08c1b277c03ed3f143fe186127a54df3c38e
mathfuru <mathfuru@gmail.com>
2012-12-22 17:02:14 +0900

    modify Apodidae::Prehtml#tag for passing the test

--- lib/apodidae/prehtml.rb
+++ lib/apodidae/prehtml.rb
@@ -4,31 +4,27 @@ module Apodidae
   class Prehtml
     def initialize(dsl_src)
       @sandbox = Sandbox.new
-      @sandbox.instance_eval(dsl_src)
+      @value = @sandbox.instance_eval(dsl_src)
     end
     def to_html
-      @sandbox.val
+      @sandbox.value
     end
-    def val
-      @sandbox.val
+    def value
+      @sandbox.value
     end
   end
 
   class Sandbox
-    attr_reader :val
+    attr_reader :value
 
     def tag(name, attrs, &block)
-      @val = if block_given?
-        #{:tag => name, :attrs =>
-      else
-        HtmlTag.new(name, {}, [])
-      end
+      @value = {:tag => name.to_s, :attrs => attrs, :inner => block_given? && block.call}
     end
 
     def to_html(flat_or_multiline=:multiline)
       raise ArgumentError, "tag's second argument must be :m(ultiline) or :f(lat)"  unless %w{multiline flat m f}.include?(flat_or_multiline.to_s)
 
-      @val = if block_given?
+      @value = if block_given?
         case flat_or_multiline.to_s[0..0]
         when 'f'
           "<#{name}>#{block.call}</#{name}>"
--- spec/units/prehtml_spec.rb
+++ spec/units/prehtml_spec.rb
@@ -6,7 +6,7 @@ describe Apodidae::Prehtml do
     describe 'when foo' do
       specify do
         Apodidae::Prehtml.new(<<-EOS).value.should ==
-          tag(:div, :class => "foo", :id => "bar") do
+          tag(:div, :class => 'foo', :id => 'bar') do
             tag(:span, :class => "foo2", :id => "bar2") do
               "baz"
             end
@@ -25,7 +25,7 @@ describe Apodidae::Prehtml do
 
       specify do
         Apodidae::Prehtml.new(%Q!tag(:span, :'data-foo' => 123){'baz'}!).value.
-          should == {:tag => 'span', :attrs => {:'data-foo' => '123'}, :inner => 'baz'}
+          should == {:tag => 'span', :attrs => {:'data-foo' => 123}, :inner => 'baz'}
       end
     end
   end
